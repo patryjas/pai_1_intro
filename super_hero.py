da3738c013778a1f5bca1087b7381d762e2c9dd4
patryjas <patrykjas08@gmail.com>
2023-05-07 18:52:26 +0200

    Removed duplicate file

--- super_hero.py
+++
@@ -1,9 +0,0 @@
-import json
-import urllib.request
-
-url = "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json"
-response = urllib.request.urlopen(url)
-data = json.loads(response.read())
-
-for hero in data['members']:
-    print(hero['name'])
