82b73d4273c18d9f90f94389b0664704dbe1d0cd
patryjas <patrykjas08@gmail.com>
2023-05-07 11:43:58 +0200

    Added exception to py code

--- JSONfromWEB.py
+++ JSONfromWEB.py
@@ -1,9 +1,16 @@
 from urllib.request import urlopen
 import json
+import requests
 
 url = "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json"
 
 response = urlopen(url)
 data_json = json.loads(response.read())
 
-print(data_json)
+
+try:
+    r = requests.get("https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json")
+    print(data_json)
+
+except requests.ConnectionError:
+    print("Connection timeout")
